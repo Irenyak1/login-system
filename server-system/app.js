c1e63008712986ef7d3cef626080625c5db6ef24
Irenyak1 <inyakate@gmail.com>
2019-08-25 00:06:50 +0300

    Add fully commented files

--- server-system/app.js
+++ server-system/app.js
@@ -1,4 +1,4 @@
-/*Defining variables and assigning them the required libraries, hte  */
+/*Defining variables and assigning them the required libraries needed to run the application */
 const express = require("express");
 const morgan = require("morgan");
 const parser = require("body-parser");
@@ -11,37 +11,50 @@ const app = express();
 // Creating a  variable html_dir that stores the path to the folder with static files
 var html_dir = './public/';
 
+
+// Setting the app to use the different libraries
 app.use(morgan("short"));
 app.use(parser.urlencoded({ extended: false }));
 app.use(express.static("./public"));
 app.use(session({
-  secret: 'secret',// Don't expose this information to another person
-  resave: true,// keeps saving sessions and can tell how many times someone has logged into the system
-  saveUninitialized: true // listens and saves even sessions that are not successfully logged in.
+  // This helps to not expose one's information to another person
+  secret: 'secret',
+  // This keeps saving sessions and can tell how many times someone has logged into the system
+  resave: true,
+  // This listens and saves even sessions that are not successfully logged in.
+  saveUninitialized: true
 }));
 
-
+// Route to fetch the index page
 app.get('/', function (req, res) {
   res.sendfile(html_dir + 'index.html');
 });
 
+// Route to fetch the register page
 app.get('/register', (req, res) => {
   res.sendfile(html_dir + 'register.html');
 });
 
+// Route to fetch the login page
 app.get('/login', (req, res) => {
   res.sendfile(html_dir + 'login.html');
 });
 
+// Route to fetch the home page
 app.get('/home', (req, res) => {
   if (req.session.loggedin) {
+    // Response/ message that one sees when they have successfully logged in
     res.send('Welcome back, ' + req.session.username + '!');
   } else {
+    // Response/ message that one sees when they want to access the home page
+    // but have not successfully logged in
     res.send('Please login to view this page!')
   }
+  // Ending of session
   res.end();
 });
 
+// Creating connection to the database
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -49,11 +62,13 @@ const connection = mysql.createConnection({
   password: 'Sweetmom1844.'
 });
 
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected!');
-// });
+// Message to show connection status
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
+// Route to capture and post data using the registration form
 app.post("/userdetails", (req, res) => {
   let object = {
     userid: req.body.userId,
@@ -66,9 +81,11 @@ app.post("/userdetails", (req, res) => {
     gender: req.body.myRadios,
     language: req.body.myChecks,
     about: req.body.notes,
-    hash:crypto.createHash('md5').update(req.body.password).digest('hex')
+    //Hashing/encrypting the password entered at signup
+    hash: crypto.createHash('md5').update(req.body.password).digest('hex')
   };
 
+  //Inserting data into the database 
   const querrystring = "INSERT INTO register(userid,pass,name,address,country,zip,email,gender,lang,about) VALUES(?,?,?,?,?,?,?,?,?,?)";
   connection.query(querrystring,
     [
@@ -84,42 +101,61 @@ app.post("/userdetails", (req, res) => {
       object.about
     ]);
 
+  //Printing out the object in a json format
   console.log("All the form inputs are" + JSON.stringify(object));
+  //Redirecting the user to the login page after registering
   res.redirect('/login')
+  //Ending the session
   res.end();
 });
 
-
+//Route to post login data
 app.post("/auth", (req, res) => {
 
+  // Requesting the username and password and assigning them to variables
+  // usernam and passw respectively
   var usernam = req.body.username;
   var passw = req.body.password;
 
+  //Hashing or encrypting the password as it is being typed at login
   var hash = crypto.createHash('md5').update(req.body.password).digest('hex')
-  
+
+  //Defining a varible querrystring to store the sql query 
+  // 'SELECT * FROM register WHERE name =? AND pass=?'
   let querrystring = 'SELECT * FROM register WHERE name =? AND pass=?'
 
+  //Condition to loop through users to find that with the specified username and password
   if (usernam && passw) {
-    // connection.query('SELECT * FROM register WHERE name =? AND pass=?', [usernam, passw], (error, results,fields)=>{ // this is a direct querry.
+    //Creating a query on the database 
     connection.query(querrystring, [usernam, hash], (error, results, fields) => {
-      if (results.length > 0) { /* you can also use if(results) or use if(results.length !== 0)*/
+      // Condition to loop through users to find one with the specified username and password
+      if (results.length > 0) {
+        //In case one has logged in
         req.session.loggedin = true;
+        //Request the username
         req.session.username = usernam;
+        //Redirect to the home page
         res.redirect('/home')
+        //Statement executed only if the user has not provided the right credentials
       } else {
         res.send('incorrect username and /or password')
       }
-      res.end(); // this can not be put inside the above else because it will terminate the session
+      //Ending session
+      res.end();
     });
-
+    //Statement executed if the user has not entered a username and/password
   } else {
     res.send('please enter username and password')
+    //Ending session
     res.end();
-  }// res.end(); can also be put after this brace
+  }
+  //Printing the username and hashed password to the console
   console.log('captured ' + usernam + " " + hash)
 
 });
 
+// Port at which the app is running
 app.listen(3002);
 
+//Printing a message to the console to show on which port the app is running
 console.log("App running at Port 3002");
--- server-system/public/regist.js
+++ server-system/public/regist.js
@@ -1,30 +1,51 @@
 /* 
-   Function to validate all the inputs, radio buttons and check box 
+   The arrow function validate to validate all the inputs, radio buttons and check box 
    in the html file and return a valid response to help the 
    user know where they have inputed a wrong value. If the value provided 
    is wrong, the input will turn red and if the value is right, it will 
    turn green.
 */
 const validate = () => {
-    //Validating user Id making sure it is between 5 to 12 characters.
+    
+    // Getting user id input field from the document by its id and 
+    // assigning it to variable myId.
     const myId = document.getElementById("userId");
+
+    // The regular expression rule to check if the user id is between 5 to 12 characters
     let idregex = /^\w{5,12}$/;
+
+    //Defining a variable IdValid and assigning it false
     let IdValid = false;
+
+    // Adding an event listener that is triggered when an input 
+    // is typed. 
     myId.addEventListener("input", e => {
-        // Method not to drop the already typed characters.
+
+        //The preventDefault() method cancels the event if it is cancelable so that 
+        // the default action that belongs to the event will not occur.
         e.preventDefault();
 
+        //The condition to check if the input value does not violate the id regular expression
         if (idregex.test(myId.value) == false) {
+
+            //Setting the style for the variable myId
             myId.style.border = "1px solid yellow";
+            //In case the id value doesn't match the regular expression, variable 
+            //IdValid is returned as false
             IdValid = false;
-        } else {
+
+        } 
+        // The else statement is instead executed in case the id value matches the regular expression
+        else {
             myId.style.border = "1px solid green";
             IdValid = true;
         }
     });
 
-    //Validating password to make sure it is between 7 to 12 characters.
+    // Getting pass input field from the document by its id and 
+    // assigning it to variable pass.
     const pass = document.getElementById("passw");
+    // The regular expression rule to check if the password is between 7 and 12 characters
     let passregex = /^\w{7,12}$/;
     let passValid = false;
     pass.addEventListener("input", e => {
@@ -39,8 +60,10 @@ const validate = () => {
         }
     });
 
-    //Validating name making sure it contains only alphabetes.
+    // Getting the name input field from the document by its id and 
+    // assigning it to variable nam.
     const nam = document.getElementById("nme");
+    // regular expression rule to check if the name contains only alphabetes
     const myName = /^[A-Za-z]+$/;
     let namVal = false;
     nam.addEventListener("input", e => {
@@ -54,11 +77,12 @@ const validate = () => {
             namVal = false;
         }
     });
-
-    //Validating country selection making sure one country is selected.
+    // Getting country input field from the document by its id and 
+    // assigning it to variable selects.
     const selects = document.getElementById("selector");
     let selectValid = false;
     selects.addEventListener("input", () => {
+        //Condition to check if a country is selected
         if (selects.value) {
             selects.style.border = "1px solid green";
             selectValid = true;
@@ -68,8 +92,10 @@ const validate = () => {
         }
     });
 
-    //Validating zipcode to make sure it is only numeric.
+    // Getting zipcode input field from the document by its id and 
+    // assigning it to variable myZip.
     const myZip = document.getElementById("zip");
+    // The regular expression rule to check if the zipcode contains only numeric
     const zipcod = /^[0-9]+$/;
     let zipVal = false;
     myZip.addEventListener("input", e => {
@@ -84,8 +110,10 @@ const validate = () => {
         }
     });
 
-    // Validating email to make sure it follows the right email pattern.
+    // Getting email input field from the document by its id and 
+    // assigning it to variable myMail.
     const myMail = document.getElementById("mail");
+    // The regular expression rule to check if the email has a valid format
     const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var emailValid = false;
     myMail.addEventListener("input", e => {
@@ -100,16 +128,15 @@ const validate = () => {
         }
     });
 
-    /* 
-        Getting checkbox input fields from the documents by their ids and
-        assigning them to variables eng and nonEng.
-    */
+    
+    // Getting checkbox input fields from the documents by their ids and
+    // assigning them to variables eng and nonEng.
     const eng = document.getElementById("eng");
     const nonEng = document.getElementById("non-eng");
 
     //An event listener triggered when the english checkbox is checked
     eng.addEventListener("change", () => {
-        //This checks if the english checkbox is checked and if so then the
+        //Condition to check if the english checkbox is checked and if so then the
         //non english checkbox will be disabled.
         if (eng.checked == true) {
             nonEng.checked = false;
@@ -118,21 +145,23 @@ const validate = () => {
 
     //An event listener triggered when the non-english checkbox is checked
     nonEng.addEventListener("change", () => {
-        // This checks if the nonenglish checkbox is checked and if so then the
+        // Condition to check if the nonenglish checkbox is checked and if so then the
         //english checkbox will be disabled.
         if (nonEng.checked == true) {
             eng.checked = false;
         }
     });
 
-    /* 
-        Method to add an event listener to the submit button of the form to return a valid 
-        response in case an input is not filled correctly.
-    */
     
+    /* 
+        This adds an event listener to the form that is triggered when the submit 
+        button is clicked. The if condition checks if the specified variable is false 
+        and then styles the input and also returns a corresponding message to the element. 
+        Else if the condition is not false then no message is returned
+    */
     document.getElementById("the-form").addEventListener("submit", e => {
         e.preventDefault();
-
+       
         if (IdValid == false) {
             myId.style.border = "1px solid red";
             document.getElementsByClassName("message")[0].innerHTML =
@@ -193,10 +222,9 @@ const validate = () => {
             document.getElementsByClassName("message")[5].innerHTML = "";
         }
 
-        /* 
-            Getting radio button input fields from the document by their ids and 
-            assigning them to variables male and female.
-        */
+        
+        // Getting radio button input fields from the document by their ids and 
+        // assigning them to variables male and female.
         const male = document.getElementById("mal");
         const female = document.getElementById("femal");
         //Checking if a radio button is selected or none
@@ -208,10 +236,8 @@ const validate = () => {
             document.getElementsByClassName("message")[6].innerHTML = "";
         }
 
-        /* 
-            This loops through the group checkboxes to check if both of them are not checked and
-            if both are not checked then a message is return to select one of them 
-        */
+        // This loops through the group checkboxes to check if both of them are not checked and
+        // if both are not checked then a message is return to select one of them 
         if (nonEng.checked == false && eng.checked == false) {
             document.getElementsByClassName("message")[7].innerHTML = "Required.";
             document.getElementsByClassName("message")[7].style =
@@ -222,5 +248,5 @@ const validate = () => {
     });
 };
 
-//Calling the validate function.
+//Invoking the validate function.
 validate();
